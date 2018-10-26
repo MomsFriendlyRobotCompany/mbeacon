91ba6c0ae0bdaa942bb83a0c8913151a97f43b22
Kevin <walchko@users.noreply.github.com>
2018-10-26 13:27:16 -0600

    Update transport.py

--- mbeacon/transport.py
+++ mbeacon/transport.py
@@ -4,6 +4,29 @@ try:
 except ImportError:
     import json
 
+"""
+dumps -> serialize
+loads -> deserialize
+
+Ascii may have to always convert data to a string first: str(data)
+
+For cross language (python/C/C++) you need to keep it simple and
+probably just an array is best for the messages.
+
+Pickle ------------------------------------------------------
+>>> v=pickle.dumps((1,2,3,"hi"))
+>>> v
+b'\x80\x03(K\x01K\x02K\x03X\x02\x00\x00\x00hiq\x00tq\x01.'
+>>> pickle.loads(v)
+(1, 2, 3, 'hi')
+
+Json --------------------------------------------------------
+>>> v=json.dumps((1,2,3,"hi"))
+>>> v
+'[1, 2, 3, "hi"]'
+>>> json.loads(v)
+[1, 2, 3, 'hi']
+"""
 
 class Ascii(object):
     """Simple ASCII format to send info"""
