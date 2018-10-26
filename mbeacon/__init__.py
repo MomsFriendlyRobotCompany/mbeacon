fd5e59a1e5d796a3c270b47f43e4f34b49886ea7
Kevin <walchko@users.noreply.github.com>
2018-10-26 09:00:32 -0600

    Create __init__.py

---
+++ mbeacon/__init__.py
@@ -0,0 +1,10 @@
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2018 Kevin Walchko
+# see LICENSE for full details
+##############################################
+from __future__ import absolute_import, print_function, division
+
+__author__ = "Kevin Walchko"
+__license__ = "MIT"
+__version__ = "0.0.1"
